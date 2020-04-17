e9fe030a4d0213e511c49881d910dff28ffec333
Yannis <bouy1920@usherbrooke.ca>
2020-04-17 18:57:22 +0200

    Test add btton

--- app/src/main/java/com/example/loup/MainActivity.kt
+++ app/src/main/java/com/example/loup/MainActivity.kt
@@ -2,11 +2,26 @@ package com.example.loup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TableLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button_dynamic = Button(this)
+        val button_dynamic1 = Button(this)
+        val button_dynamic2 = Button(this)
+        val button_dynamic3 = Button(this)
+        val button_dynamic4 = Button(this)
+        val button_dynamic5 = Button(this)
+        grillePerso.addView(button_dynamic)
+        grillePerso.addView(button_dynamic1)
+        grillePerso.addView(button_dynamic2)
+        grillePerso.addView(button_dynamic3)
+        grillePerso.addView(button_dynamic4)
+        grillePerso.addView(button_dynamic5)
     }
 }
